e1b27d9da9d06e2a9013c4714d51181d5374b9db
Brian Muchunku <brauxer@gmail.com>
2021-09-21 03:51:24 +0300

    Create the Pizza Order page section by adding Bootstrap's Navbar to be the launch point of our Pizza Ordering Process and adding Cards to hold each of instance of our Pizza Variety Object

--- main.js
+++ main.js
@@ -37,9 +37,6 @@ for which has been designated to it to achieve.
 // This implies that the prices according to size will be fixed for all
 // the various Pizza varieties.
 
-// We then use the Object 'Prototype' method to set the initial value 
-// of the price component, the initial value of the crust to be selected 
-// and the initial value of the toppings to be selected.
 
 // Lets now define the components interacting with the Pizza Object,
 // that will enable the Pizza Object to generate its expected outcomes.
@@ -53,7 +50,70 @@ for which has been designated to it to achieve.
            i.e. Stuffed, Crispy and Gluten Free
 
     iv)Toppings: The topping types available and their prices:
-            i.e. Anchovies, Pepperoni, Chicken       
+            i.e. Anchovies, Pepperoni, Chicken    
+
+            
+
+            
+// We then use the Object 'Prototype' method to set the initial value 
+// of the price component, the initial value of the crust to be selected 
+// and the initial value of the toppings to be selected as follows:
 
 */
 
+PizzaVariety.prototype.price= 0;
+PizzaVariety.prototype.crust= null;
+PizzaVariety.prototype.topping=[];
+
+// We then define the Constructors to create the Crusts and Toppings Objects 
+// whose properties we shall use to complete each Customers customized Pizza
+// and determine the additional cost of customization
+
+function Crusts(name, price){
+
+      this.name = name;
+      this.price = price;
+}
+
+function Toppings(name, price){
+
+      this.name= name;
+      this.price=price;
+
+}
+// According to our Business logic, given that our Brianitos Pizza customers 
+// can purchase multiple Pizza Varieties each with their varying options,
+// create a Cart object using a cart Constructor function to manage 
+// each Customer's order according to the array of Pizza Variety items in the cart 
+// and the optional cost of delivery where applicable. 
+
+// We define our Cart using a Constructor Function as follows:
+
+
+function Cart(){
+
+    const cart = this;
+    this.cartItems= [];
+    this.delivery= null;
+    this.addToCart= function(item){
+      cart.cartItems.push(item);
+     $("#cartItems").html(cartItems.length);
+
+    }
+}
+
+// Introduce a new Zone Object to determine the cost of delivery according 
+// to the respective zone within which each order delivery is to be made.
+
+
+
+function Zone(zoneName, price){
+
+      this.zoneName = zoneName;
+      this.price = price;
+
+}
+
+let cart = new Cart();
+let selectedPizza;
+let cartItemHtml;
